fcee1ff53f229270acab949f2e2ee527ec7a0d85
Cavallo Victor Gabriel <victor.cavallo@naranja.com>
2021-05-10 11:04:54 -0300

    add ingress and java deployment

--- Terraform/modules/helm_release/main.tf
+++ Terraform/modules/helm_release/main.tf
@@ -1,7 +1,9 @@
 resource "helm_release" "jenkins" {
-  name       = "jenkins"
-  repository = "https://marketplace.azurecr.io/helm/v1/repo"
-  chart      = "jenkins"
+  name             = "jenkins"
+  repository       = "https://marketplace.azurecr.io/helm/v1/repo"
+  chart            = "jenkins"
+  namespace        = "jenkins"
+  create_namespace = true
 
   set {
     name  = "jenkinsUser"
@@ -11,4 +13,18 @@ resource "helm_release" "jenkins" {
     name  = "jenkinsPassword"
     value = var.jenkinsPassword
   }
+}
+
+resource "helm_release" "nginx" {
+  name             = "nginx"
+  repository       = "https://helm.nginx.com/stable"
+  chart            = "nginx-ingress"
+  namespace        = "ingress-basic"
+  timeout          = "900"
+  create_namespace = true
+
+  values = [
+    "${file("helm_values/nginx_values.yaml")}"
+  ]
+
 }
